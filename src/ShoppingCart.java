6f8c71c97065155df5d6d68a9411030b67a543a5
OsamaB <osama.benhabara@live.se>
2014-12-25 12:32:24 +0100

    Create Item and ShoppingCart classes

---
+++ src/ShoppingCart.java
@@ -0,0 +1,26 @@
+
+public class ShoppingCart
+{
+	Item[] items = new Item[10];
+	
+	public void addItem(Item addThis){
+		
+	}
+	
+	public void removeItem(Item removeThis){
+		
+	}
+	
+	public Item[] getItems(){
+		return items;
+	}
+	
+	public int getSum(){
+		int sum = 0;
+		return sum;
+	}
+	
+	public String toString(){
+		return "";
+	}
+}
