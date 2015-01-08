9e1c0923dc3ee509fca55d91f39063907d2240fc
Stefan De Geer <sdgkeff@gmail.com>
2015-01-08 13:59:22 +0100

    clearat

--- Shop/src/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-import java.util.HashMap;
-
-public interface OrderRepository{
-    public void addOrder(Customer customer);
-    public Order getOrder(String key);
-    public HashMap<String,Order> getAllOrders();    
-}
